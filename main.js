2affe835c3d54a16718e49f18a18e342768e3e37
Gaetan CREN <gaetan.cren@easymile.com>
2024-05-13 11:51:05 +0200

    TD2

--- main.js
+++ main.js
@@ -7,9 +7,6 @@ export function add(stringNumber){
         return 0
     try {
         const arr = stringNumber.split(",")
-        if (arr.length>2){
-            return "Erreur : que 2 chiffres bon sang ! "
-        }
         let number = 0
         arr.forEach(element => {
             if (isNaN(parseInt(element))){
@@ -20,7 +17,7 @@ export function add(stringNumber){
         if (arr.length == 1){
             return parseInt(arr[0])
         }
-        return parseInt(arr[1]) + parseInt(arr[0])
+        return number
     } catch (e){
         return "Il ne faut rentrer que des chiffres !!!"
     } 
--- main.test.js
+++ main.test.js
@@ -18,5 +18,5 @@ test("1 chiffre", () => {
 })
 
 test("trop long", () => {
-  expect(add("1,2,3")).toBe("Erreur : que 2 chiffres bon sang ! ")
+  expect(add("1,2,3")).toBe(6)
 })
